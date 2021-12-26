0d91339d410d53799423dd2803f8f9f36df414ae
Yashaswi Anand <yash.kwr17@gmail.com>
2021-12-27 00:12:30 +0530

    Stack

--- Stack/BracketBalance.java
+++ Stack/BracketBalance.java
@@ -1,7 +1,5 @@
 package Stack;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -35,6 +33,7 @@ public class BracketBalance {
 		String brackets = sc.next();
 		Boolean check = checkBracketBalance(brackets);
 		System.out.println(check);
+		sc.close();
 		
 		
 
