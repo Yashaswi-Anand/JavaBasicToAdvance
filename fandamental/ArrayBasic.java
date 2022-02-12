2d17204312a348f1dd1aae569fb792d9cf14fe38
Yashaswi Anand <yash.kwr17@gmail.com>
2022-02-12 12:46:27 +0530

    array

---
+++ fandamental/ArrayBasic.java
@@ -0,0 +1,34 @@
+package fandamental;
+
+import java.util.*;
+
+public class ArrayBasic {
+	
+	public static void main(String args[]) {
+		
+		int a[][] = new int[2][0];
+//		a[1][0] = 1;
+//		
+//		for(int i=0;i<a.length;i++) {
+//			for(int j =0;j<a[i].length;j++) {
+//				System.out.println(a[i][j]);
+//			}
+//		}
+		
+		//System.out.println(a[0].length +" " + a[1].length);
+		
+//	1) abcde
+//		2) bcdf
+//		3) pqr
+//		4) abei
+		
+		System.out.println((int)'a'+(int)'b'+(int)'c'+(int)'d'+(int)'e'); //495
+		System.out.println((int)'b'+(int)'c'+(int)'d'+(int)'f'); //399
+		System.out.println((int)'p'+(int)'q'+(int)'r'); //339
+		System.out.println((int)'a'+(int)'m'+(int)'i'+(int)'t'); //401
+		System.out.println(427%26);
+		
+		
+	}
+
+}
